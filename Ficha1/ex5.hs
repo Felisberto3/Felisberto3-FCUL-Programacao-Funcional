9cc784d2b04c860b3dc4368ab7317ab04341f05d
Felisberto Alberto <felisbertoalberto183@gmail.com>
2026-03-12 21:08:36 +0000

    beforeLast2

--- Ficha1/ex5.hs
+++ Ficha1/ex5.hs
@@ -1,1 +1,14 @@
+-- verifica se o tamanho da list é maior que 10
+-- @params list é uma list
+-- @return True se a lista é maior que 10 e False caso contrario
 funcaoMaiorQue10 list = length list > 10
+
+isVazio list = null list
+
+takeOutElement a = init (tail a)
+
+secondElement list = list !! 1
+
+beforeLast list = last (init list)
+
+beforeLast2 list = list !! (length list - 2)
