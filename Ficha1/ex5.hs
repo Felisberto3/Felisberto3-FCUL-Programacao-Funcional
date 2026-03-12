44b139f709b1a4c9b5618133ed12efc9c8d7eee1
Felisberto Alberto <felisbertoalberto183@gmail.com>
2026-03-12 21:53:32 +0000

    ex5.hs terminando as alineas a) a j)

--- Ficha1/ex5.hs
+++ Ficha1/ex5.hs
@@ -1,18 +1,24 @@
 -- verifica se o tamanho da list é maior que 10
 -- @params list é uma list
 -- @return True se a lista é maior que 10 e False caso contrario
+-- a)
 funcaoMaiorQue10 list = length list > 10
 
+-- b)
 isVazio list = null list
 
+-- c)
 takeOutElement a = init (tail a)
 
+-- d)
 secondElement list = list !! 1
 
+-- e)
 beforeLast list = last (init list)
 
 beforeLast2 list = list !! (length list - 2)
 
+-- f)
 listElement list n = list !! (n - 1)
 
 -- rewrite
@@ -24,3 +30,18 @@ beforeLast3 = last . init
 -- secondElement
 secondElement2 :: [c] -> c
 secondElement2 = head . tail
+
+-- g)
+reverte list = head list : reverse (tail list)
+
+-- h)
+somatorio list = sum (take 5 list)
+
+-- i)
+somatorio2 list n = sum (take n list)
+
+-- j)
+func list1 list2 =
+  last list1 == last list2
+    && length list1 == length list2
+    && not (null list1 && null list2)
