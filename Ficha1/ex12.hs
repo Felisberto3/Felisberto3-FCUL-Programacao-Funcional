915e92b5711ae6a193121496e164d0edbaaa9ce8
Felisberto Alberto <felisbertoalberto183@gmail.com>
2026-03-29 16:22:03 +0100

    ex12.hs

---
+++ Ficha1/ex12.hs
@@ -0,0 +1,20 @@
+-- a
+perfeito n = [i | i <- [1 .. n - 1], mod n i == 0]
+
+factores n = factoresAux n 1
+  where
+    factoresAux n i
+      | i >= n = []
+      | mod n i == 0 = i : factoresAux n (i + 1)
+      | otherwise = factoresAux n (i + 1)
+
+perfeitos' lim = perfeitosAux lim 1
+  where
+    perfeitosAux lim i
+      | i >= lim = []
+      | i == sum (factores i) = i : perfeitosAux lim (i + 1)
+      | otherwise = perfeitosAux lim (i + 1)
+
+-- calcular of factores e somatorio de forma recursiva.
+-- b
+perfeitos n = [i | i <- [1 .. n], sum (perfeito i) == i]
--- Ficha1/ex7.hs
+++ Ficha1/ex7.hs
@@ -3,4 +3,5 @@
 -- onde a primeira partição é dada pelos primeiros dois elementos da lista,
 -- a segunda partição pelo segundo e terceiro elementos da lista, e assim
 -- adiante. Por exemplo:
+particao :: (Enum a, Fractional a) => a -> a -> a -> [a]
 particao a b n = [a + ((b - a) / n) * x | x <- [0 .. n]]
