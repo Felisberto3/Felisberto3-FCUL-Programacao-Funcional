5ab101facb035707d0cd5f4ca46b57e2b71dfe10
Felisberto Alberto <felisbertoalberto183@gmail.com>
2026-03-29 19:38:23 +0100

    ex20.hs

--- Ficha1/ex20.hs
+++ Ficha1/ex20.hs
@@ -1,8 +1,6 @@
-import Data.Type.Coercion (trans)
-
 transp :: [[a]] -> [[a]]
 transp [] = []
 transp ([] : _) = []
 transp m = map head m : transp (map tail m)
 
-multM m1 m2 = [[sum (zipWith (*) ml mr)] | ml <- m1, mr <- transp m2]
+multM m1 m2 = [[sum (zipWith (*) ml mr) | mr <- transp m2] | ml <- m1]
