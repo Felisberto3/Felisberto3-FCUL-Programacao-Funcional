b718ed53d3c76465c1c3c1c209aeebd2d8de72a5
Felisberto Alberto <felisbertoalberto183@gmail.com>
2026-04-06 00:01:59 +0100

    elem'

--- Ficha4/ex1.hs
+++ Ficha4/ex1.hs
@@ -8,3 +8,13 @@ replicate' n x
     | n < 0 = []
     | n == 1 = [x]
     | otherwise  = x:replicate' (n-1) x
+
+maximo:: Ord a => [a] -> a
+maximo [x] = x
+maximo (x:xs) 
+    | x> maximo(xs)  = x
+    | otherwise = maximo(xs)
+
+elem' :: Eq a => a -> [a] -> Bool
+elem' _ []  = False
+elem' n (x:xs) =  x == n  || elem' n (xs)
