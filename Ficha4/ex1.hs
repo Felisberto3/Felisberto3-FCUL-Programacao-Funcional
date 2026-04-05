2df8acdd17cc2e07b695d4e19d2f62d05332c5b3
Felisberto Alberto <felisbertoalberto183@gmail.com>
2026-04-06 00:15:58 +0100

    substitui, altera e elem functions

--- Ficha4/ex1.hs
+++ Ficha4/ex1.hs
@@ -1,3 +1,4 @@
+-- a)
 soma'::Num a => [a] -> a
 soma' [] = 0
 soma' (x:xs) = x + soma' xs
@@ -9,12 +10,29 @@ replicate' n x
     | n == 1 = [x]
     | otherwise  = x:replicate' (n-1) x
 
+-- c)
 maximo:: Ord a => [a] -> a
 maximo [x] = x
 maximo (x:xs) 
     | x> maximo(xs)  = x
     | otherwise = maximo(xs)
 
+-- -d)
 elem' :: Eq a => a -> [a] -> Bool
 elem' _ []  = False
 elem' n (x:xs) =  x == n  || elem' n (xs)
+
+-- e)
+substitui:: Eq a => a-> a-> [a] -> [a]
+substitui _ _ [] = []
+substitui v1 v2 (x:xs) 
+    | v1 == x  = v2: substitui v1 v2 xs
+    | otherwise = x:substitui v1 v2 xs
+
+-- f)
+altera:: Ord a => [a] -> a-> a -> [a]
+altera [] _ _ = []
+altera (x:xs) a1 a2 
+    |  x < a1 = a2: altera xs a1 a2
+    | otherwise = x:altera xs a1 a2
+
