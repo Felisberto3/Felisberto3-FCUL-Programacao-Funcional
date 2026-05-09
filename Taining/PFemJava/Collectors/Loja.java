cbc58533ee9d8ea15bd4045df233547e802e2ab9
Felisberto Alberto <felisbertoalberto183@gmail.com>
2026-05-09 23:41:10 +0100

    Usar Predicate and all e any

---
+++ Taining/PFemJava/Collectors/Loja.java
@@ -0,0 +1,28 @@
+package Taining.PFemJava.Collectors;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Loja {
+    public record Pessoa(String name, Integer idade) {
+    }
+
+    public static void main(String[] args) {
+        List<Pessoa> list = List.of(
+                new Pessoa("Felisberto", 23),
+                new Pessoa("Kalenhe", 23),
+                new Pessoa("Alberto", 32),
+                new Pessoa("Mukinda", 45),
+                new Pessoa("Condumula", 12));
+
+        Predicate<Pessoa> f = x -> x.idade % 2 == 0;
+        Predicate<Pessoa> g = x -> x.idade % 2 == 1;
+        boolean todasIdadesPares = list.stream().allMatch(f);
+        boolean todasIdadesImpares = list.stream().allMatch(g);
+
+        System.out.println("Todas idades Pares: " + todasIdadesPares);
+        System.out.println("Todas idades Impares: " + todasIdadesImpares);
+
+    }
+}
