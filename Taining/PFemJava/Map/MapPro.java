353f1d1c48cd270c5e6ba7bdfa0d03856c034100
Felisberto Alberto <felisbertoalberto183@gmail.com>
2026-05-10 21:23:07 +0100

    MapPro

---
+++ Taining/PFemJava/Map/MapPro.java
@@ -0,0 +1,31 @@
+package Taining.PFemJava.Map;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class MapPro {
+    public record Pessoa(String name, int idade) {
+    }
+
+    public static void main(String[] args) {
+        List<Pessoa> list = List.of(
+                new Pessoa("Felisberto", 17),
+                new Pessoa("Alberto", 23),
+                new Pessoa("Kelenhe", 15),
+                new Pessoa("Mukinda", 3),
+                new Pessoa("Norberto", 60));
+
+        Map<String, Pessoa> mapPessoa = list.stream().collect(Collectors.toMap(Pessoa::name, (Pessoa x) -> x));
+
+        System.out.println("==========================");
+        System.out.println("======== Testando  =========");
+
+        mapPessoa.keySet().forEach(x -> System.out.println(mapPessoa.get(x)));
+
+        Map<String, Integer> mapNomeIdade = list.stream().collect(Collectors.toMap(Pessoa::name, Pessoa::idade));
+        System.out.println("====== Testando Map (Nome e Idade) ======");
+        mapNomeIdade.keySet().forEach(x -> System.out.println(mapNomeIdade.get(x)));
+
+    }
+}
