421499bb7fe746a1dd69a719f811d6993014f4cf
Felisberto Alberto <felisbertoalberto183@gmail.com>
2026-05-14 17:44:11 +0100

    ex4

--- Ficha5/ex4.hs
+++ Ficha5/ex4.hs
@@ -2,3 +2,12 @@ zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
 zipWith' _ _ [] = []
 zipWith' _ [] _ = []
 zipWith' f (x : xs) (y : ys) = f x y : zipWith' f xs ys
+
+-- b) ------------------Com Lista em compreensao-----------
+zipWith'' :: (a -> a -> a) -> [a] -> [a] -> [a]
+zipWith'' f list1 list2 = [f (list1 !! i) (list2 !! i) | i <- [0 .. min (length list1) (length list2) - 1]]
+
+-- c) -------------------- Alinea C
+zip' :: [a] -> [b] -> [(a, b)]
+-- essa é a forma mais simples => zip'  = zipWith (\x y -> (x, y))
+zip' = zipWith (,)
