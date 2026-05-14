13803b2cd8a4d51ed33f9d85ca754b1cfcc4cdf4
Felisberto Alberto <felisbertoalberto183@gmail.com>
2026-05-14 17:31:18 +0100

    clean

--- Ficha7/ex1.hs
+++
@@ -1,3 +0,0 @@
-maxWeight :: [([[Float]], [Float])] -> Float
-maxWeight ((a, b) : xs) =
-  foldl (\acc (f, s) -> maximum [maximum $ map maximum f, (maximum s), acc]) (max (maximum $ map maximum a) (maximum b)) xs
