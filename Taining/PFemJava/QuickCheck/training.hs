18f6ee6e9986a563fea303a33e4d6fde1c4949d3
Felisberto Alberto <felisbertoalberto183@gmail.com>
2026-05-17 23:49:47 +0100

    instancia de Arbitrary

--- Taining/PFemJava/QuickCheck/training.hs
+++ Taining/PFemJava/QuickCheck/training.hs
@@ -1,6 +1,16 @@
 import Test.QuickCheck
 import Test.QuickCheck (Property)
 
+-- stdArgs::Args
+-- stdArgs = Args
+-- {
+--     maxSuccess = 100,
+--     maxDiscard = 500,
+--     maxSize = 100,
+--     chatty = True,
+--     replay = Nothing,
+-- }
+
 soma :: (Num a, Eq a) => a -> Bool
 soma x = x + 1 == x + 2
 
@@ -36,3 +46,31 @@ prop_verificarSeMaiorq2 :: Property
 prop_verificarSeMaiorq2 = property $ do
   list <- gerarLista2ouMais
   return $ length list >= 2
+
+prop_reverseClassify :: [Int] -> Property
+prop_reverseClassify xs =
+  classify (null xs) "Lista Vazia" $
+    classify (length xs < 10) "Lista menores q 10" $
+      classify (length xs > 10) "Lista maiores q 10" $
+        reverse (reverse xs) == xs
+
+prop_reverseCollect :: [Int] -> Property
+prop_reverseCollect xs =
+  collect (length xs) $
+    reverse (reverse xs) == xs
+
+data Cor = Vermelho | Verde | Azul | Branco deriving (Show, Eq, Enum)
+
+instance Arbitrary Cor where
+  arbitrary = elements [Vermelho, Verde, Azul, Branco]
+
+gerarCor :: Gen [Cor]
+gerarCor = do
+  a <- arbitrary
+  b <- arbitrary
+  c <- arbitrary
+  return (a : c : b)
+
+prop_corRever :: Property
+prop_corRever =
+  forAll gerarCor (\xs -> length xs >= 28)
